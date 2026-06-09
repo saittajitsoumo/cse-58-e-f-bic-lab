cfa6b68acea123838a283e5781b00fd69ab683a6
saittajitsoumo <157392103+saittajitsoumo@users.noreply.github.com>
2026-06-09 16:13:11 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
+dna = input().strip()
 
+complement = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C'
+}
+
+reverse_complement = ""
+
+for nucleotide in reversed(dna):
+    reverse_complement += complement[nucleotide]
+
+print(reverse_complement)
