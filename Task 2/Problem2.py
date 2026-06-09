e2fa7bf92e7a9df4101547ba1b8108ef39c462d2
saittajitsoumo <157392103+saittajitsoumo@users.noreply.github.com>
2026-06-09 16:07:01 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,10 @@
+text="ACGTTGCATGTCGCATGATGCATGAGAGCT"
+k=4
 
+counts ={}
+for i in range(len(text)-k+1):
+  kmer = text[i:i+k]
+  counts[kmer]=counts.get(kmer,0)+1
+
+max_count = max(counts.values())
+print(" ".join(k for k,v in counts.items() if v ==max_count))
