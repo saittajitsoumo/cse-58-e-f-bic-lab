d3215367ecc34d4ecb6afadac30b0129938c9606
saittajitsoumo <157392103+saittajitsoumo@users.noreply.github.com>
2026-06-16 16:22:58 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,21 @@
+pattern = input()
+text = input()
+d = int(input())
 
+ans = []
+
+k = len(pattern)
+
+for i in range(len(text) - k + 1):
+    substring = text[i:i+k]
+
+    mismatch = 0
+
+    for j in range(k):
+        if pattern[j] != substring[j]:
+            mismatch += 1
+
+    if mismatch <= d:
+        ans.append(i)
+
+print(*ans)
